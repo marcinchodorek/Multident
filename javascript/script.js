e0fd5d38e74da5e3d505b0b86ebdcbaaaba767d5
Marcin Chodorek <marcin.chodorek3@gmail.com>
2021-02-24 22:05:38 +0100

    Added hero content Added slider to hamburger menu Styled hero content

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,21 @@
 const hamburger = document.querySelector('.navbar__burger-box');
 const navigation = document.querySelector('.navbar__navigation');
-
+const textPath = document.querySelectorAll('.hero__text-svg path');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('burger--active');
     navigation.classList.toggle('navbar__navigation--active');
-})
+});
+
+setTimeout(() => {
+    textPath.forEach(path => {
+        path.style.strokeDasharray = 0;
+    })
+}, 3200)
+
+window.addEventListener('keydown', (e) => {
+    console.log(e.keyCode);
+});
+
+
+
